8b7eb50321b13d0693357cde4efa0cf41172fc3c
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-11-13 21:17:03 +0000

    remove elements

---
+++ src/dsa/two_pointers/slow_fast_pointers/remove_elements.py
@@ -0,0 +1,16 @@
+
+def remove_elements(arr: list[int], val:int) -> list[int]:
+    
+    slow = 0
+    for fast in range(len(arr)):
+        if arr[fast] != val:
+            arr[slow] = arr[fast]
+            slow += 1
+    return slow    # we return the number of elements in the list
+    
+    
+if (__name__ == "__main__"):
+    nums = [1,1,2,2,3]
+    val = 1
+    length = remove_elements(nums, val)
+    print(length, nums[:length])                    # 3 elements with list == [2, 2, 3]
