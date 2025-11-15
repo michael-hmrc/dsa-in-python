19e56181c04a5d0c3d58f035b4435f4a65bdc177
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-11-15 07:34:07 +0000

    wip

--- src/neetcode150/arrays_and _hash_maps/two_sum.py
+++ src/neetcode150/arrays_and _hash_maps/two_sum.py
@@ -1,6 +1,6 @@
 class TwoSum:     
 
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+    def two_sum(self, nums: list[int], target: int) -> list[int]:
         seen = {}
         for i, num in enumerate(nums): 
             diff = target - num
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     solution = TwoSum()
     nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]   # list is 0 indexed
     target = 5
-    print(solution.twoSum(nums, target))  # [1, 2]
+    print(solution.two_sum(nums, target))  # [1, 2]
