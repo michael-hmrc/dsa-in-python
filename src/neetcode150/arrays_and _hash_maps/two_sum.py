6f1273e13c034f4bda3dd235527a6edbb04f54e5
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-11-15 07:19:44 +0000

    clean up

--- src/neetcode150/arrays_and _hash_maps/two_sum.py
+++ src/neetcode150/arrays_and _hash_maps/two_sum.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     solution = TwoSum()
     nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]   # list is 0 indexed
     target = 5
-    print(solution.twoSum(nums, target))  # [1,2]
+    print(solution.twoSum(nums, target))  # [1, 2]
