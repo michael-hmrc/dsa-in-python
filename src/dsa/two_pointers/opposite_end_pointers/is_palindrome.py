4952ec758a3b275da4de03be24cd6dc2502d9190
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-12-08 23:00:54 +0000

    wip

--- src/dsa/two_pointers/opposite_end_pointers/is_palindrome.py
+++ src/dsa/two_pointers/opposite_end_pointers/is_palindrome.py
@@ -1,4 +1,4 @@
-# two pointer technique
+#### Two-pointer technique
 
 def is_palindrome(s: str) -> bool:
     left = 0
@@ -9,7 +9,35 @@ def is_palindrome(s: str) -> bool:
             return False
         left += 1
         right -= 1
-    return True        
+    return True   
+
+
+#### Leetcode 125
+
+def valid_palindrome(s: str) -> bool:
+    
+    left = 0
+    right = len(s) - 1
+    
+    while left < right:
+        
+        # 1. Skip non-alphanumeric characters
+        while (left < right) and not s[left].isalnum():
+            left += 1
+        
+        while left < right and not s[right].isalnum():
+            right -= 1
+        
+        # 2. Compare lowercase versions
+        if s[left].lower() != s[right].lower():
+            return False
+        
+        left += 1
+        right -= 1
+    
+    return True
+
+     
 
 if __name__ == "__main__":
     
@@ -20,3 +48,7 @@ if __name__ == "__main__":
     print(is_palindrome(palindrome_string))
     print(is_palindrome(palindrome_string2))
     print(is_palindrome(not_palindrome_string))
+    
+    print(valid_palindrome(palindrome_string))
+    print(valid_palindrome(palindrome_string2))
+    print(valid_palindrome(not_palindrome_string))
